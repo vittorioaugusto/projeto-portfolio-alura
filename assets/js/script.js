a6c69d205064fdd5ee3a73ea2e3d23402c9c0c00
vittorioaugusto <vi.aborges47@gmail.com>
2024-06-28 13:42:10 -0300

    modificações no css e js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
-document.getElementById('alterar_tema').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const body = document.body;
     const toggleIcon = document.querySelector('#alterar_tema i')
 
-    body.classList.toggle('light-theme')
-
-    if (body.classList.contains('light-theme')) {
-        toggleIcon.classList.remove('fa-moon');
+    const theme = localStorage.getItem('theme')
+    if (theme === 'light') {
+        body.classList.add('light-theme')
+        toggleIcon.classList.remove('fa-moon')
         toggleIcon.classList.add('fa-sun')
     } else {
+        body.classList.remove('light-theme')
         toggleIcon.classList.remove('fa-sun')
         toggleIcon.classList.add('fa-moon')
     }
+
+    document.getElementById('alterar_tema').addEventListener('click', function () {
+        body.classList.toggle('light-theme')
+
+        if (body.classList.contains('light-theme')) {
+            toggleIcon.classList.remove('fa-moon')
+            toggleIcon.classList.add('fa-sun')
+            localStorage.setItem('theme', 'light')
+        } else {
+            toggleIcon.classList.remove('fa-sun')
+            toggleIcon.classList.add('fa-moon')
+            localStorage.setItem('theme', 'dark')
+        }
+    })
 })
